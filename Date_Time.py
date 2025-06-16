c7889141a25e14602348fc97ce062b5118e6e1f9
Yehouda-12 <coheny748@gmail.com>
2025-06-16 20:04:52 +0300

    Update Date_Time.py

--- Date_Time.py
+++ Date_Time.py
@@ -1,10 +1,11 @@
+#קוד מייבא את מחלקת datetime(תאריך ושעה)
 from datetime import datetime
 
 
-#הקוד מייבא את מחלקת datetime, שומר את התאריך והשעה הנוכחיים במשתנה, ומדפיס אותם למסך.
+#הקוד  שומר את התאריך והשעה הנוכחיים במשתנה כולל שניות וחלקי שניות, ומדפיס אותם למסך
 now = datetime.now()
 print(now)
 
-#שומר רק שנה.חודש.יום.שעה.דקה
-now = datetime.now().strftime("%Y-%m-%d %H:%M")
+# שומר רק שנה.חודש.יום.שעה.דקה ומדפיס אותם למסך
+now = datetime.now().strftime("%Y-%m-%d %H:%M")#Y=year m=month d= day H=hour M=minut
 print(now)
